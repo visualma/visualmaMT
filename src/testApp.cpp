ae846cc0c6b8d89912135e093121f41b170c91b6
Josue Page Vizcaino <pv.josue@gmail.com>
2012-09-20 13:48:33 -0500

    Funcion procesarHoughesP(ofCvColorImage img,int posX,int posY) agregada.

---
+++ src/testApp.cpp
@@ -0,0 +1,357 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    ofEnableSmoothing(); 
+    
+  //valores iniciales
+    g_iFotos = 0;
+	g_iNLineas = 20;
+	g_iGris = 255;
+	g_iThreshold = 30;
+    tipoFiltro = 1;
+    modoOperacion = 1;
+	g_iAnchoLineas = height/g_iNLineas;
+    max_imageW = 400;
+    width = ofGetViewportWidth();
+	height = ofGetViewportHeight();
+    
+    modoOperacion = "Image";
+    initImagen();
+    
+      
+  //renderGui
+    generaGui();
+    generaGuiRt();
+    generaAtajos();
+    
+    cambiaEstilo("dark");
+    
+}
+
+void testApp::initImagen() {
+    Imagen.loadImage("1.jpg");
+    imgOutput.allocate(Imagen.width,Imagen.height);
+}
+void testApp::initVideo() {
+    for(int i=0;i<nFotos;i++)
+    {
+        img[i].allocate(CAM_WIDTH*2,CAM_HEIGHT*2);
+        imgOutput.allocate(img[i].width,img[i].height);
+    }
+	vidGrabber.setVerbose(true);
+	vidGrabber.initGrabber(CAM_WIDTH,CAM_HEIGHT);
+	
+}
+
+//--------------------------------------------------------------
+void testApp::update() {
+    mg->addPoint(ofGetFrameRate());
+    
+    if(modoOperacion == "Live WebCam") {
+		if(vidGrabber.isInitialized())
+			vidGrabber.grabFrame();
+    } else if(modoOperacion == "Image") {
+        //imgUpdate
+    } else if(modoOperacion == "3D Model") {
+        //3dUpdate
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    if(modoOperacion == "Live WebCam") {
+        procesaVideo();
+    } else if(modoOperacion == "Image") {
+        procesaImagen();
+    } else if(modoOperacion == "3D Model") {
+        //procesa3d();
+    }
+}
+
+void testApp::procesaImagen() {
+    float newHeight = Imagen.getHeight()*(max_imageW/Imagen.getWidth());
+    
+	img[0].setFromPixels(Imagen.getPixels(),Imagen.width,Imagen.height);
+	imgOutput = img[0];
+	imgOutput.invert();
+	imgOutput.threshold(g_iThreshold);
+	imgOutput.invert();
+	imgOutput.resize(max_imageW, newHeight);
+    Imagen.resize(max_imageW, newHeight);
+    ofSetHexColor(0xffffff);
+	Imagen.draw(256,0);
+	//imgOutput.draw(imgOutput.width+256,0);
+    int* a = procesaHoughesP(img[0],256,0);
+}
+
+void testApp::procesaVideo() {
+
+	img[0].setFromPixels(vidGrabber.getPixels(),vidGrabber.width,vidGrabber.height);
+	//draw(img[0].width+256,0);
+	ofSetColor(255);
+	vidGrabber.draw(256,0);
+	int* lineas = procesaHoughesP(img[0],256,0);
+     for(int i=0;i<g_iFotos;i++)
+         img[i].draw(i*CAM_WIDTH,CAM_HEIGHT);
+     for(int i=0;i<g_iNLineas;i++) {
+         if(i%2==0)
+             ofRect(width/2,i*g_iAnchoLineas,0,width/2,g_iAnchoLineas);
+     }
+}
+
+
+int* testApp::procesaHoughesP(ofxCvColorImage img,int posX,int posY)
+{
+    float newHeight = img.getHeight()*(max_imageW/img.getWidth());
+	ofxCvGrayscaleImage imgOut;
+	imgOut = img;
+	imgOut.invert();
+	imgOut.threshold(g_iThreshold);
+	imgOut.invert();
+	if(imgOut.width>max_imageW)
+		imgOut.resize(max_imageW, newHeight);
+	//IplImage* imgData = imgOut.getCvImage();
+	IplImage* imgCanny = cvCreateImage(cvSize(imgOut.width,imgOut.height),8,1);
+	cvCanny(imgOut.getCvImage(),imgCanny,0,50,5);
+	int maxLinesNumber = 1000;
+	int *lines = new int[maxLinesNumber*4];
+	int threshold = 5;
+	int lineLenght = 10;
+	int pixelGap = 2;
+	int linesNumber = cvHoughLinesP(imgCanny, 1, 0.01745, threshold,lineLenght , pixelGap, lines, maxLinesNumber);
+	ofPoint P1, P2;
+	P1.y = 0;
+	posX+=imgOut.width;
+	imgOut.draw(posX,posY);
+	ofSetColor(255,0,0);
+	for(int l=0;l<linesNumber;l++) 
+	{
+		P1.x = (int)lines[ 4*l]+posX;
+		P1.y = (int)lines[ 4*l+1];//+posY;
+		P2.x = (int)lines[ 4*l+2]+posX;
+		P2.y = (int)lines[ 4*l+3];//+posY;
+		ofLine(P1,P2);
+	}
+	return lines;
+}
+
+//-------------------------------------------------   RENDER GUI
+
+//------------------------------- Sidebar izquierda
+void testApp::generaGui(){
+    float dim = 16; 
+	float xInit = OFX_UI_GLOBAL_WIDGET_SPACING; 
+    float length = 255-xInit; 
+	
+    gui = new ofxUIScrollableCanvas(0, 0, length+xInit, ofGetHeight()); 
+    
+    gui->addWidgetDown(new ofxUIMultiImageButton(128/2, 129/2, false, "GUI/images/icoVisualma128.png", "Visualma",OFX_UI_FONT_LARGE));
+    //gui->addWidgetRight(new ofxUILabel(100,0,"Visualma", OFX_UI_FONT_LARGE),OFX_UI_ALIGN_RIGHT); 
+    gui->addSpacer(length-xInit, 1);
+    
+    vector<string> names;
+	names.push_back("HoughLines");
+	names.push_back("Algoritmo 2");
+	names.push_back("Alg3");	
+	gui->addRadio("Filtro", names, OFX_UI_ORIENTATION_VERTICAL, dim, dim, 0, 32); 	
+    gui->addSpacer(length-xInit, 2);
+    
+    gui->addWidgetDown(new ofxUILabel("# Lineas", OFX_UI_FONT_SMALL)); 
+	gui->addSlider("numLineas", 0, 1000, &g_iNLineas, length-xInit,dim);
+    gui->addSlider("anchoLineas", 0, 200, &g_iAnchoLineas, length-xInit,dim, OFX_UI_FONT_SMALL);
+    
+    
+    gui->addSpacer(length-xInit, 2);
+	gui->add2DPad("PAD", ofPoint(0,length-xInit), ofPoint(0,120), ofPoint((length-xInit)*.5,120*.5), length-xInit,120);
+    
+    gui->addSpacer(length-xInit, 2);			
+    gui->addWidgetDown(new ofxUIRotarySlider(dim*4, 0, 100, 50, "Angulo")); 			
+    gui->addSpacer(length-xInit, 2);
+    
+    vector<string> items; 
+    items.push_back("Live WebCam"); 
+    items.push_back("Image"); 
+    items.push_back("3D Model"); 
+    gui->addDropDownList("Modo de operacion", items, 200);
+  
+    ofAddListener(gui->newGUIEvent,this,&testApp::guiEvent);
+}
+
+//--------------------------------  Sidebar derecha
+void testApp::generaGuiRt(){
+    float dim = 16; 
+	float xInit = OFX_UI_GLOBAL_WIDGET_SPACING; 
+    float length = 255-xInit; 
+    
+    guiRt = new ofxUIScrollableCanvas(ofGetWidth()-length+xInit, 0, length+xInit, ofGetHeight());     
+    guiRt->loadSettings("GUI/guiSettings.xml"); 
+    
+    guiRt->addWidgetDown(new ofxUILabel("Herramientas", OFX_UI_FONT_SMALL)); 	
+    guiRt->addWidgetRight(new ofxUIImageToggle(dim, dim, true, "GUI/images/Preview.png","FULLSCREEN")); 
+    guiRt->addSpacer(length-xInit, 2);
+        
+    guiRt->addWidgetDown(new ofxUILabel("INput", OFX_UI_FONT_SMALL)); 			
+    guiRt->addWidgetDown(new ofxUIRotarySlider((length-xInit)*.25, 0, 250, &g_iGris, "Luminosidad")); 			
+    guiRt->addWidgetRight(new ofxUIRotarySlider((length-xInit)*.25, 0, 500, &g_iThreshold, "Threshold"));  
+    guiRt->addSpacer(length-xInit, 1);                                
+    
+    guiRt->addWidgetDown(new ofxUIToggleMatrix(dim*2, dim*2, 4, 4, "Capas")); 
+    
+    guiRt->addFPSSlider("FPS", length-xInit, dim);
+    vector<float> buffer; 
+    for(int i = 0; i < 256; i++)
+        buffer.push_back(0.0);				    
+    mg = (ofxUIMovingGraph *) guiRt->addWidgetDown(new ofxUIMovingGraph(length-xInit, 120, buffer, 256, 0, 400, "MOVING GRAPH"));
+	guiRt->addSpacer(length-xInit, 0);	
+    
+    
+    guiRt->addWidgetDown(new ofxUIMultiImageButton(dim, dim, true, "GUI/images/toggle.png", "light"));
+    guiRt->addWidgetRight(new ofxUIMultiImageToggle(dim, dim, false, "GUI/images/toggleB.png", "dark"));
+    
+    guiRt->addToggle("ShortCuts", false, dim, dim);
+    ofAddListener(guiRt->newGUIEvent,this,&testApp::guiEvent);
+    
+}
+
+//-------------------------- Lista de ShortCuts
+void testApp::generaAtajos(){
+    float dim = 16; 
+	float xInit = OFX_UI_GLOBAL_WIDGET_SPACING; 
+    float length = 255-xInit; 
+    
+    atajos = new ofxUIScrollableCanvas(ofGetWidth()-(length)+(xInit), ofGetHeight()-300, length+xInit, 300);  
+    
+    atajos->addWidgetDown(new ofxUILabel("Atajos", OFX_UI_FONT_MEDIUM)); 	
+    atajos->addWidgetDown(new ofxUILabel("[f] FullScreen", OFX_UI_FONT_SMALL)); 	
+    atajos->addWidgetDown(new ofxUILabel("[h] Show/hide Tools", OFX_UI_FONT_SMALL));
+    atajos->addWidgetDown(new ofxUILabel("[f] FullScreen", OFX_UI_FONT_SMALL)); 	
+    atajos->toggleVisible(); 
+}
+
+void testApp::posicionaGui() {
+    float xInit = OFX_UI_GLOBAL_WIDGET_SPACING; 
+    float length = 255-xInit; 
+    gui->setRectParent(new ofxUIRectangle(0, 0, length+xInit, ofGetHeight()));
+    guiRt->setRectParent(new ofxUIRectangle(300, 0, length+xInit, ofGetHeight()));
+    atajos->setRectParent(new ofxUIRectangle(ofGetWidth()-(length)+(xInit), ofGetHeight()-300, length+xInit, 300));
+}
+
+//------------------------------------------------------  UTIL
+
+void testApp::cambiaEstilo(string estilo) {
+    if (estilo == "dark"){
+            ofBackground(51, 51, 51);
+            gui->setTheme(OFX_UI_THEME_MACOSX);
+            guiRt->setTheme(OFX_UI_THEME_MACOSX);
+    } else
+    if (estilo == "light"){
+            ofBackground(240, 240, 240); 
+            gui->setTheme(OFX_UI_THEME_MINBLACK);
+            guiRt->setTheme(OFX_UI_THEME_MINBLACK);
+    }
+}
+
+//------------------------------------------------  Eventos GUI
+
+void testApp::guiEvent(ofxUIEventArgs &e)
+{
+    string name = e.widget->getName(); 
+	int kind = e.widget->getKind(); 
+	cout << "got event from: " << name << endl; 
+    
+    
+    if(name == "FULLSCREEN"){
+        ofxUIImageToggle *toggle = (ofxUIImageToggle *) e.widget; 
+        ofSetFullscreen(toggle->getValue()); 
+    } else if(name == "numLineas") {
+        ofxUISlider *slider = (ofxUISlider *) e.widget;    
+        //modificar numero de lineas
+        g_iNLineas = slider->getScaledValue();
+    } else if(name == "Tipo de filtro") {
+
+    } else if(name == "ShortCuts") {
+         atajos->toggleVisible(); 
+    } else if(name == "Modo de operacion") {
+        ofxUIDropDownList *ddlist = (ofxUIDropDownList *) e.widget;
+        vector<ofxUIWidget *> &selected = ddlist->getSelected(); 
+        if(selected.size() > 0){
+            modoOperacion = selected[0]->getName();
+            cout << "ModoOperacion: " << selected[0]->getName() << endl; 
+            if(modoOperacion == "Live WebCam") {
+                initVideo();
+            } else if(modoOperacion == "Image") {
+                initImagen();
+            } else if(modoOperacion == "3D Model") {
+                //init3d();
+            }
+        }
+    } else if(name == "dark") {
+        cambiaEstilo("dark");
+    } else  if(name == "light") {
+        cambiaEstilo("light");
+    }
+}
+
+
+//--------------------------------------------  Eventos globales
+void testApp::keyPressed(int key){
+    switch (key) {
+		case 'f':
+			ofToggleFullscreen(); 
+			break;
+        case 'h':
+            gui->toggleVisible(); 
+            guiRt->toggleVisible(); 
+			break;  
+        case OF_KEY_UP:
+            g_iNLineas++;
+            break;
+        case OF_KEY_DOWN:
+            if(g_iNLineas>0)
+                g_iNLineas--;
+            break;
+        case OF_KEY_RIGHT:
+            g_iGris++;
+            break;
+        case OF_KEY_LEFT:
+            if(g_iGris>0)
+                g_iGris--;
+            break;
+    }
+}
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+}
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y){
+}
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+}
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+}
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+}
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+    posicionaGui();
+}
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+}
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+}
+
+
+//--------------------------------------------------------  EXIT
+void testApp::exit()
+{
+    gui->saveSettings("GUI/guiSettings.xml");     
+    delete gui; 
+    delete guiRt;
+}
+//--------------------------------------------------------------
---
+++ src/testApp.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxUI.h"
+#include "ofxOpenCv.h"
+
+#define CAM_WIDTH 320
+#define CAM_HEIGHT 240
+#define nFotos 3
+class testApp : public ofBaseApp{
+	public:
+		void setup();
+		void update();
+		void draw();
+        void exit(); 
+    
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    
+        // GUI
+        void generaGui();
+        void generaGuiRt();
+        void generaAtajos();
+        void posicionaGui();
+        void cambiaEstilo(string estilo);
+        void guiEvent(ofxUIEventArgs &e);
+
+        ofxUICanvas *gui;        
+        ofxUICanvas *guiRt;
+        ofxUICanvas *atajos;
+        ofxUIMovingGraph *mg; 
+        float *buffer; 
+        ofImage *imagen; 
+    
+    
+        // Logica
+        void initVideo();
+        void initImagen();
+        void procesaVideo();
+        void procesaImagen();
+		int* procesaHoughesP(ofxCvColorImage img,int posX,int posY);
+        ofxCvColorImage img[nFotos];
+        ofxCvGrayscaleImage imgOutput;
+        ofVideoGrabber vidGrabber;
+        ofImage Imagen;
+        float g_iAnchoLineas,g_iNLineas, width, height,g_iGris,g_iThreshold,max_imageW;
+        int    numLineas, tipoFiltro,g_iFotos;
+        string modoOperacion;
+};
